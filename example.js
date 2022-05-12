9172e1b3fe5b377509cb833e66f43fc245782cb7
John Whitten <johnwhitten.studio@gmail.com>
2022-05-12 16:49:36 -0700

    completed functions as data, continued

--- example.js
+++ example.js
@@ -44,3 +44,28 @@
 // const fetchData = DEVELOPMENT
 //     ? fetchDataFake
 //     : fetchDataReal;
+
+const double = x => x * 2;
+const subtractOne = x => -1;
+const triple = x => x * 3;
+const add5 = x => x + 5;
+
+const myNumber = 42;
+
+const doubled = double(myNumber);
+const minusOne = subtractOne(doubled);
+//..
+
+const functionsArray = [
+    double, 
+    subtractOne,
+    triple,
+    add5,
+    Math.sqrt,
+  ];
+
+var number = 42;
+
+functionsArray.forEach(func => number = func(number));
+
+console.log(number);
