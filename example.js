1fb79eccdccb32f2535635f02b710578892e3270
John Whitten <johnwhitten.studio@gmail.com>
2022-05-12 16:13:29 -0700

    first-class function exercises

---
+++ example.js
@@ -0,0 +1,46 @@
+// const add = (x, y) => x + y;
+
+// const double = number => number * 2;
+// //as opposed to
+// // const double = function(number) {
+// //   return number * 2;
+// // }
+
+// const sayHello = () => console.log('Hello');
+
+// const getPersonData = () => ({
+//     name: 'John Whitten',
+//     age: 38,
+//     job: 'programmer',
+// });
+
+// const myArrowFunction = (arg1, arg2) => arg1 + arg2;
+
+// as opposed to
+// const myArrowFunction = (arg1, arg2) => {
+//   return arg1 + arg2;
+// }
+
+// const sayHello = name => console.log('Hello' + name);
+
+// const sayHello2 = sayHello;
+// sayHello2('John');
+
+// const myFunction = true 
+//     ? () => console.log('First option')
+//     : () => console.log('Second option');
+
+// const DEVELOPMENT = true;
+
+// const fetchDataReal = () => {
+//   // time-intensive operations here
+// }
+
+// const fetchDataFake = () => ({
+//     name: 'John Whitten',
+//     age: 38,
+// });
+
+// const fetchData = DEVELOPMENT
+//     ? fetchDataFake
+//     : fetchDataReal;
