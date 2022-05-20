36d94d6ee12af49e696353cd25ba1be63d83730e
John Whitten <johnwhitten.studio@gmail.com>
2022-05-20 16:38:55 -0700

    complete first challenge in section 5

--- exampleCh04.js
+++ exampleCh04.js
@@ -12,6 +12,9 @@ const { validateUseBuiltInsOption } = require("@babel/preset-env/lib/normalize-o
 // "useful way to configure more general functions into more specific ones"
 
 
+// Solve the following functions in a # of different ways
+//-----V----V----V------
+
 // const add = (x, y, z) => x + y + z;
 
 // const addPartial = x => 
@@ -54,16 +57,15 @@ const { validateUseBuiltInsOption } = require("@babel/preset-env/lib/normalize-o
 
 // ------ OR -----
 
-const add = (x, y, z) => x + y + z;
+// const add = (x, y, z) => x + y + z;
 
-const addPartial = x =>
-    y =>
-        z => add(x, y, z);
+// const addPartial = x =>
+//     y =>
+//         z => add(x, y, z);
 
-const sum = addPartial(5)(6)(7);
+// const sum = addPartial(5)(6)(7);
 
-console.log(sum);
-
+// console.log(sum);
 
 
 
@@ -72,10 +74,89 @@ console.log(sum);
 // Recursion
 // -----------------------------------------------
 
+// Recursion: a case when a function calls itself
 
+// We always have to tell our function when to stop!!
+
+// A simple example using recursion, creating a function that behaves like a for loop without using a for loop
+
+
+
+// const countDown = x => {
+//     if (x < 0) return;
+//     console.log(x);
+//     countDown(x-1);
+// };
+
+// countDown(10);
+
+// const countUp = (x, max) => {
+//   if (x > max) return;
+//   console.log(x);
+//   countUp(x + 1, max);
+// };
+
+// countUp(0, 10);
 
 
 
 
 // Functions as objects
 // -----------------------------------------------
+
+// we mean functions have properties like objects do
+
+// const sayHello = () => console.log('Hello');
+// console.log(sayHello.name);
+
+// const add = (x, y, z) => x + y + z;
+// console.log(add.length);
+
+
+// --- call ---
+// const add = (x, y, z) => x + y + z;
+// console.log(add.call(null, 1, 2, 3));
+
+// --- apply ---
+// const add = (x, y, z) => x + y + z;
+// console.log(add.apply(null, [1, 2, 3]));
+
+// --- (...) spread operator ---
+// const add = (x, y, z) => x + y + z;
+// const args = [1, 2, 3];
+// console.log(add(...args));
+
+// --- bind ---
+// const add = (x, y, z) => x + y + z;
+// const args = [1, 2, 3];
+// const add1 = add.bind(null, 1)
+// console.log(add1(2, 3));
+
+
+
+
+
+// Challenge: Convert Array
+// -----------------------------------------------
+
+
+const electionVotes = [
+  'Harry', 'Rick', 'Ben', 'Ben', 'Fran', 'Katherine', 'Morgan', 'Fran', 'Ashley', 'Connor', 'Katherine', 'Rick', 'Fran', 'Rick', 'Albert', 'Ben', 'Sarah', 'Morgan', 'Fran', 'John', 'Albert', 'Donna', 'John', 'Katherine', 'Mortimer', 'Harry', 'Frank', 'Fran', 'Morgan', 'Katherine', 'John', 'Ben', 'Bill', 'Julie', 'Fran'
+];
+
+const tallyVotes = votes => 
+  votes.reduce((acc, name) => ({
+      ...acc, 
+      [name]: acc[name] ? acc[name] + 1 : 1,
+  }), {});
+
+
+console.log(tallyVotes(electionVotes));
+
+/* Expected Output (something like this):
+    {
+        Fran: <some number>
+        John: <some number>
+        Katherine: <some number>
+    }
+*/
